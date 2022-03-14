2b825691745237ffd4fc0e5ce9a24a1213b6a258
paul88vera <paul88vera@gmail.com>
2022-03-14 11:45:19 -0500

    completed comment route

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const pizzaRoutes = require('./pizza-routes');
+const commentRoute = require('./comment.routes');
 
 // add prefix of `/pizzas` to routes created in `pizza-routes.js`
 router.use('/pizzas', pizzaRoutes);
+router.use('/comments', commentRoute);
 
 module.exports = router;
