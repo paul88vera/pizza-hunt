fa8c63ed0b90f52b83b3cea68a5a1995cfce52d1
paul88vera <paul88vera@gmail.com>
2022-03-15 14:37:21 -0500

    issue #2 is completed

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -5,6 +5,12 @@ const pizzaController = {
 // functions will go here as methods
 getAllPizza(req, res) {
   Pizza.find({})
+  .populate({
+    path: "comments",
+    select: "-__v",
+  })
+  .select('-__v')
+  .sort({ _id: -1 })
   .then(dbPizzaData => res.json(dbPizzaData))
   .catch(err => {
     console.log(err);
@@ -14,6 +20,10 @@ getAllPizza(req, res) {
 // get one pizza by id
   getPizzaById({ params }, res){
     Pizza.findOne({ _id: params.id })
+    .populate({
+      path: 'comments',
+      select: '-__v',
+    })
     .then(dbPizzaData => {
       // if no pizza is found, send 404
       if (!dbPizzaData) {
