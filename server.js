15fc8e53d4f870402014d40c744484384d77d514
paul88vera <paul88vera@gmail.com>
2022-03-16 22:30:29 -0500

    useFindAndModify commented out

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static("public"));
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost:27017/pizza-hunt",
   {
-    useFindAndModify: false,
+    // useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
